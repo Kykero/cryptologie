32c29f1e9ef5dd13b18f448c0afd6483398f2c1c
Kykero <notanemail@exemple.com>
2025-04-17 15:49:09 +0200

    TP César question 1-4

---
+++ Cesar/Cesar.py
@@ -0,0 +1,59 @@
+
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+            'P','Q','R','S','T','U','V','W','X','Y','Z']
+
+def encodage(message, k):
+    code = []    
+    for a in message:
+        if a != ' ':
+            if a in alphabet:
+                # Récupère l'indice de la lettre dans l'alphabet
+                index = alphabet.index(a)
+                # Cyclique avec le modulo
+                new_index = (index + k) % len(alphabet)
+                code.append(alphabet[new_index])
+            else:
+                # Si le caractère n'est pas dans l'alphabet, on le laisse inchangé
+                code.append(a)
+        else:
+            # Conserve les espaces dans le message
+            code.append(' ')
+    return ''.join(code) # join permet de faire une seule chaîne pour la phrase ça rend juste le résultat plus lisible
+
+def decryptage(message,k):
+    message_decrypte = []
+    for a in message :
+        if a != ' ':
+            if a in alphabet:
+                #On récupère l'indice dans l'alphabet
+                index = alphabet.index(a)
+                #Indice Cyclique
+                new_index = (index - k) % len(alphabet)
+                message_decrypte.append(alphabet[new_index])
+            else:
+                #On converve les espaces
+                message_decrypte.append(a)
+        else:
+            #on garde les espaces
+            message_decrypte.append(' ')
+    return ''.join(message_decrypte)
+
+
+print("Pour la question 1, un cercle serait une forme géometrique mieux adapté pour représenter l'alphabet du code César l'alphabet")
+print(' ------------ ')
+
+message_encode = encodage('ABC', 25)
+print(message_encode)
+
+print(' ------------ ')
+message_decrypte = decryptage('YHQL YLGL YLFL',3)
+print(message_decrypte)
+
+print(' ------------ ')
+for k in range(0,25):
+    message_decrypte3 = decryptage('MOVK OCD ZVEC PKMSVO AEO ZBOFE',k)
+    print(message_decrypte3)
+
+print(' ------------ ')
+message_decrypte4 = decryptage('MOVK OCD ZVEC PKMSVO AEO ZBOFE',10)
+print(message_decrypte4)
